f593a1deb0bd0aacbdc5cb571b5324cebb1cc46c
AlishuangA <2513858497@qq.com>
2020-10-24 18:33:57 +0800

    m

---
+++ precache-manifest.0144ab5f201b48cf98b36800ee24fb94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9a2fe6825b7f91641191bf84aa11a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01cbe284753e4dc4dfa4",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "5ea0f8ceac2464e37193",
+    "url": "/static/css/main.f775259c.chunk.css"
+  },
+  {
+    "revision": "01cbe284753e4dc4dfa4",
+    "url": "/static/js/2.6c1091c5.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.6c1091c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea0f8ceac2464e37193",
+    "url": "/static/js/main.02465fcd.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "8870703fa3b960c10e7c614f551fb650",
+    "url": "/static/media/goods.8870703f.jpg"
+  }
+]);
